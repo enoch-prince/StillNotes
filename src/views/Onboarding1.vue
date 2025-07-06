29b496a46ad76bd3cab9247367440469c9f46389
enoch-prince <63630599+enoch-prince@users.noreply.github.com>
2025-07-06 01:22:23 +0000

    styled buttons

--- src/views/Onboarding1.vue
+++ src/views/Onboarding1.vue
@@ -1,97 +1,133 @@
 <script setup lang="ts">
-import { useOnboardingStore } from "@/stores/counter";
-import Onboard1SVG from "@/components/svgs/onboard1SVG.vue";
-import Onboard2SVG from "@/components/svgs/onboard2SVG.vue";
-import Onboard3SVG from "@/components/svgs/onboard3SVG.vue";
-import PaginationBullets from '@/components/PaginationBullets.vue';
+import { useOnboardingStore } from '@/stores/counter'
+import Onboard1SVG from '@/components/svgs/onboard1SVG.vue'
+import Onboard2SVG from '@/components/svgs/onboard2SVG.vue'
+import Onboard3SVG from '@/components/svgs/onboard3SVG.vue'
+import PaginationBullets from '@/components/PaginationBullets.vue'
 
-const onboard = useOnboardingStore();
-
+const onboard = useOnboardingStore()
 </script>
 
 <template>
-    <div class="flex-center max-height custom-background" v-touch:swipe.right="onboard.prevStep">
-        <div class="" v-if="onboard.step == 0">
-            <p @click="onboard.nextStep" class="is-size-2 is-family-secondary has-text-primary-100 is-clickable">StillNotes</p>
-        </div>
-        <div class="flex-center" v-else-if="onboard.step == 1">
-            <div class="is-align-content-center">
-                <Onboard1SVG />
-            </div>
-            <p class="is-size-3-mobile has-text-centered has-text-primary-100 mt-5">Find peace in the small, scattered, uncertain moments.
-            </p>
-            <div class="is-flex is-align-items-flex-start is-justify-content-space-between width-80vw mt-6">
-                <div>
-                    <button type="button">Skip</button>
-                </div>
-                <div>
-                    <button class="button" @click="onboard.nextStep">
-                        <span>Next</span>
-                        <span class="icon is-small">
-                            <font-awesome-icon icon="fa-solid fa-arrow-right"
-                                style="color:var(--bulma-primary);" />
-                        </span>
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="flex-center" v-else-if="onboard.step == 2">
-            <div class="is-align-content-center">
-                <Onboard2SVG />
-            </div>
-            <p class="is-size-3-mobile has-text-centered has-text-primary-100 mt-5">Every quiet moment matters</p>
-            <div class="is-flex is-align-items-flex-start is-justify-content-space-between width-80vw mt-6">
-                <div>
-                    <button type="button">Skip</button>
-                </div>
-                <div>
-                    <button class="button" @click="onboard.nextStep">
-                        <span>Next</span>
-                        <span class="icon is-small">
-                            <!-- <font-awesome-icon icon="fa-brands fa-google" style="color: #B197FC;" /> -->
-                            <font-awesome-icon icon="fa-solid fa-arrow-right" style="color:var(--bulma-primary);"/>
-                        </span>
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="flex-center" v-else>
-            <div class="is-align-content-center">
-                <Onboard3SVG />
-            </div>
-            <p class="is-size-3-mobile has-text-centered has-text-primary-100 mt-5">Reflect, remember, and stay rooted</p>
-            <div class="is-flex is-flex-direction-row-reverse width-80vw mt-6">
-                <div>
-                    <button class="button is-rounded" @click="onboard.nextStep()">
-                        <span class="has-text-primary">Next</span>
-                        <span class="icon is-small">
-                            <font-awesome-icon icon="fa-solid fa-arrow-right" style="color:var(--bulma-primary);"/>
-                        </span>
-                    </button>
-                </div>
-            </div>
-        </div>
-        <PaginationBullets :total-pages="3" v-show="onboard.step > 0"/>
+  <div class="flex-center max-height custom-background" v-touch:swipe.right="onboard.prevStep">
+    <div class="" v-if="onboard.step == 0">
+      <p
+        @click="onboard.nextStep"
+        class="is-size-2 is-family-secondary has-text-primary-100 is-clickable"
+      >
+        StillNotes
+      </p>
     </div>
+    <div class="flex-center" v-else-if="onboard.step == 1">
+      <div class="is-align-content-center">
+        <Onboard1SVG />
+      </div>
+      <p class="is-size-3-mobile has-text-centered has-text-primary-100 mt-5">
+        Find peace in the small, scattered, uncertain moments.
+      </p>
+      <div
+        class="is-flex is-align-items-flex-start is-justify-content-space-between width-80vw mt-6"
+      >
+        <div>
+          <button
+            class="button is-transparent is-rounded-4 px-4p5 py-4"
+          >
+         <span class="is-family-secondary has-text-primary-100">Skip</span>
+          
+          </button>
+        </div>
+        <div>
+          <button class="button is-rounded-4 px-4p5 py-4" @click="onboard.nextStep">
+            <span class="has-text-primary is-family-secondary mr-3 "
+              >Next</span
+            >
+            <span class="icon is-small ml-3">
+              <font-awesome-icon
+                icon="fa-solid fa-arrow-right"
+                style="color: var(--bulma-primary)"
+              />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="flex-center" v-else-if="onboard.step == 2">
+      <div class="is-align-content-center">
+        <Onboard2SVG />
+      </div>
+      <p class="is-size-3-mobile has-text-centered has-text-primary-100 mt-5">
+        Every quiet moment matters
+      </p>
+      <div
+        class="is-flex is-align-items-flex-start is-justify-content-space-between width-80vw mt-6"
+      >
+        <div>
+          <button
+            class="button is-transparent is-rounded-4 px-4p5 py-4"
+          >
+         <span class="is-family-secondary has-text-primary-100">Skip</span>
+          
+          </button>
+        </div>
+        <div>
+          <button class="button is-rounded-4 px-4p5 py-4" @click="onboard.nextStep">
+            <span class="has-text-primary is-family-secondary mr-3 "
+              >Next</span
+            >
+            <span class="icon is-small ml-3">
+              <font-awesome-icon
+                icon="fa-solid fa-arrow-right"
+                style="color: var(--bulma-primary)"
+              />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="flex-center" v-else>
+      <div class="is-align-content-center">
+        <Onboard3SVG />
+      </div>
+      <p class="is-size-3-mobile has-text-centered has-text-primary-100 mt-5">
+        Reflect, remember, and stay rooted
+      </p>
+      <div class="is-flex is-flex-direction-row-reverse width-80vw mt-6">
+        <div>
+          <button class="button is-rounded-4 px-4p5 py-4" @click="onboard.nextStep">
+            <span class="has-text-primary is-family-secondary mr-3"
+              >Next</span
+            >
+            <span class="icon is-small ml-3">
+              <font-awesome-icon
+                icon="fa-solid fa-arrow-right"
+                style="color: var(--bulma-primary)"
+              />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <PaginationBullets :total-pages="3" v-show="onboard.step > 0" />
+  </div>
 </template>
 
 <style scoped>
 .custom-background {
-    background: linear-gradient(-180deg, rgba(205, 194, 255, 1) 0%, rgba(167, 149, 248, 1) 100%);
+  background: linear-gradient(-180deg, rgba(205, 194, 255, 1) 0%, rgba(167, 149, 248, 1) 100%);
 }
 
 .flex-center {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
 .width-80vw {
-    width: 85vw;
+  width: 85vw;
 }
 
 .max-height {
-    height: 100vh;
+  height: 100vh;
 }
 </style>
