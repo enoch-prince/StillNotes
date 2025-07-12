f1643f0b96cde667ecd5bf61cabdfea0716aaf18
enoch-prince <63630599+enoch-prince@users.noreply.github.com>
2025-07-12 21:14:51 +0000

    added questionaire

---
+++ src/components/Questionaire.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mini-card">
+    <div class="is-flex is-flex-direction-row is-justify-content-space-between has-width-100">
+      <span class="icon icon-layout">
+        <font-awesome-icon icon="fa fa-gamepad"></font-awesome-icon>
+      </span>
+      <span class="icon is-small is-rounded-background-success">
+        <font-awesome-icon icon="fa-regular fa-circle-check" class="has-text-light"></font-awesome-icon>
+      </span>
+    </div>
+    <div>
+      <p class="is-family-secondary is-size-6-touch has-text-weight-semibold">
+        Daily Devotion
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+.mini-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0.5rem;
+  gap: 0.875rem;
+
+  width: 9.75rem;/*156px;*/
+  height: 8.5rem;/*136px;*/
+
+  background: #d1c7ff;
+  border-radius: 0.5rem;
+}
+
+
+.icon-layout {
+  display: flex;
+  padding: 0.75rem;
+  margin: 0 2rem 0 auto; /* top right bottom left */
+  width: 2.875rem;
+  height: 2.875rem;
+  background: #ffffff;
+  border-radius: 100%;
+}
+
+.is-rounded-background-success {
+    border-radius: 100%;
+    background-color: var(--bulma-success);
+}
+
+.has-width-100 {
+    width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+import Questionaire from '@/components/Questionaire.vue'
 </script>
 
 <template>
   <main>
-     <div>
-      <p class="has-container">This is the Home Page 😎</p>
-     </div>
+    <div class="px-4 pt-8">
+      <p class="is-size-4 mb-5 has-text-grey">Almost there!</p>
+      <p class="is-size-3 mb-5">What are you using StillNote for?</p>
+      <div class="is-flex columns is-touch is-multiline is-centered">
+        <div class="is-flex column">
+          <Questionaire />
+        </div>
+        <div class="is-flex column">
+          <Questionaire />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.pt-8 {
+  padding-top: 5.125rem !important;
+}
+</style>
