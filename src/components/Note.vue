12b4ff797de46bb2ca19012923fc270e7c27dcbd
enoch-prince <63630599+enoch-prince@users.noreply.github.com>
2025-07-30 00:01:59 +0000

    added note component & note view

---
+++ src/components/Note.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useNoteDraftStore } from '@/stores/counter'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router';
+
+defineProps<{ id_num?: number; fullVersion?: boolean }>()
+
+// const emit = defineEmits(['viewNoteEvent'])
+
+const notesStore = useNoteDraftStore()
+const noteTitle = ref(notesStore.title)
+const noteContent = ref(notesStore.content)
+const selectedColor = ref(notesStore.color)
+const fontStyle = ref(notesStore.font)
+const scripture = ref(notesStore.scripture)
+
+
+const router = useRouter()
+const route = useRoute()
+
+const viewNote = (id: number) => {
+  if (route.name === 'search') {
+    console.log("OnClick viewNote: id = ", id)
+    // emit('viewNoteEvent', { id: id })
+    router.push({ name: 'view-note', params: { noteId: id }})
+  }
+}
+
+</script>
+
+<template>
+  <div class="box" @click="viewNote(id_num!)">
+    <p class="has-text-centered">
+      This is a recent <strong>note {{ id_num }}</strong> !!!
+    </p>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
---
+++ src/views/SingleNoteView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import AppBar from '@/components/AppBar.vue';
+import Note from '@/components/Note.vue';
+import { onMounted, ref, watchEffect } from 'vue';
+
+// const handleViewNoteEvent = (payload: any) => {
+//     console.log("Received payload: ", payload)
+// }
+
+const props = defineProps<{noteId: string}>()
+
+onMounted(() => {
+    console.log("Passed Id as route param: ", props.noteId)
+})
+
+</script>
+
+<template>
+  <div>
+    <AppBar title="New Note">
+      <a class="navbar-item" @click="">
+        <span> <font-awesome-icon icon="fa-solid fa-ellipsis fa-lg" /> </span>
+      </a>
+    </AppBar>
+    <!-- <Note full-version @viewNoteEvent="handleViewNoteEvent"/> -->
+    <Note full-version/>
+  </div>
+</template>
+
+<style scoped></style>
