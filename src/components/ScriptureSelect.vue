f620b96b849c6820b1ffced5e1112ec60d9de6b4
enoch-prince <63630599+enoch-prince@users.noreply.github.com>
2025-08-06 00:33:28 +0000

    Modified ScriptureSelect to match UI

--- src/components/ScriptureSelect.vue
+++ src/components/ScriptureSelect.vue
@@ -3,8 +3,11 @@
 </script>
 
 <template>
-    <div class="box">
-        <p class="has-text-centered">Scripture content shows up here</p>
+    <div class="is-flex px-4">
+        <div style="margin-bottom: 1.25rem; width: 100%;">
+            <p class="is-family-secondary is-size-7" style="color: #C8C5CB;">RECENT SEARCHES</p>
+        </div>
+        <!-- Add a ScriptureBar component -->
     </div>
 </template>
 
